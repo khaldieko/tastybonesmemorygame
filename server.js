e60e91bf357aae91ce65d5dd96b11182a6f160a7
Akinwole Diekololaoluwa <91050325+khaldieko@users.noreply.github.com>
2022-01-27 04:35:07 +0100

    changes

--- server.js
+++ server.js
@@ -4,18 +4,23 @@ var webpackHotMiddleware = require("webpack-hot-middleware");
 var config = require("./webpack.config");
 var path = require("path");
 var express = require("express");
+var dotenv=require("dotenv")
+dotenv.config()
 
 var app = new (require("express"))();
-var port = 3000;
+var port = 4000;
 
 var compiler = webpack(config);
-app.use(
-  webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath,
-  })
-);
-app.use(webpackHotMiddleware(compiler));
+if (process.env.NODE_ENV==="development") {
+  app.use(
+    webpackDevMiddleware(compiler, {
+      noInfo: true,
+      publicPath: config.output.publicPath,
+    })
+  );
+  app.use(webpackHotMiddleware(compiler))
+}
+
 
 app.use(express.static(path.join(__dirname, "/")));
 app.get("/", function (req, res) {
